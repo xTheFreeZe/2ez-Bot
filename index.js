3bd1c080ac4bdcd6b487d35709752fbcdcf6b7e8
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2021-05-05 23:27:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -101,6 +101,23 @@ client.on('message', message => {
     }
 });
 
+client.on('message', message => {
+    if (message.content === '*error') {
+
+        if (message.author.id !== '420277395036176405') {
+            return console.log('Returned!');
+        }
+
+        const welcomechannel = client.channels.cache.get('585883817458401342');
+
+        welcomechannel.channel.send('An unexpected error occurred! `Value is not snowflake`');
+        welcomechannel.channel.send('check the console for more info');
+
+    }
+});
+
+
+
 
 
 
