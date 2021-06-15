06dbf95a224e2e059b1517f52801539b4493035d
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2021-06-15 20:06:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -133,12 +133,6 @@ client.on('message', message => {
     }
 });
 
-client.on('message', message => {
-    if (message.content.includes("stella")) {;
-        message.react('<:pinkgg:590571639905714187>')
-    }
-});
-
 
 client.on('message', message => {
     if (message.content === '*members') {
