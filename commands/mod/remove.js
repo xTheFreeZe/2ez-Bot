fedf5ab209c040aad7146602fe4199084d260377
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2021-06-17 21:18:41 +0200

    Update remove.js

--- commands/mod/remove.js
+++ commands/mod/remove.js
@@ -49,7 +49,7 @@ module.exports = {
             if (!member) return message.channel.send(nomember);
             //if (!roleName) return message.channel.send(norole);
 
-            const alreadyHasRole = member._roles.includes(roleName.id);
+            const alreadyHasRole = member._roles.includes(roleName);
 
             if (!alreadyHasRole) return message.channel.send('This User does not have that Role!');
 
