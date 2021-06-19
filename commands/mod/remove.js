a013565826912257dc6e43dc67d31d0e1bbd88fb
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2021-06-19 17:15:13 +0200

    Channel ID patch
    This will ensure that *give and *remove can only be used in the Team Channel!

--- commands/mod/remove.js
+++ commands/mod/remove.js
@@ -9,90 +9,97 @@ module.exports = {
 
     run: async (client, message, args) => {
 
-        let useembed = new MessageEmbed()
-            .setTitle('Incorrect Usage')
-            .setDescription('Its `<username | user id> <role name>')
-            .setColor('RED')
+        if (message.channel.id == '587956575315034114') {
 
-        let STTpermissions = new MessageEmbed()
-            .setDescription('Please give the Bot Permissions to do this!')
-            .addField('Needed', '`MANAGE_ROLES`')
-            .setColor('RED')
-
-        let nomember = new MessageEmbed()
-            .setDescription('Please mention someone, or provide an ID!')
-            .setColor('RED')
-
-        let norole = new MessageEmbed()
-            .setDescription('Please mention a role!')
-            .setColor('RED')
-
-
-
-        if (!message.guild.me.hasPermission('MANAGE_ROLES')) {
-
-            return message.channel.send(STTpermissions);
-
-        }
-
-        if (!message.channel.id == '587956575315034114') return;
-
-
-        if (!args[0] || !args[1]) return message.channel.send(useembed);
-
-
-        try {
-
-            const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-            const roleName = message.guild.roles.cache.find(r => (r.name === args[1].toString()) || (r.id === args[1].toString().replace(/[^\w\s]/gi, '')));
-
-            if (!member) return message.channel.send(nomember);
-            //if (!roleName) return message.channel.send(norole);
-
-            const alreadyHasRole = member._roles.includes(roleName.id);
-
-            if (!alreadyHasRole) return message.channel.send('This User does not have that Role!');
-
-
-            const embed = new MessageEmbed()
-                .setTitle(`${roleName.name} was removed!`)
-                .setDescription(`${roleName} was removed from ${member.user}`)
-                .setColor('f3f3f3')
-                .setTimestamp()
-                .setFooter(`${message.author.username}`)
-                .setThumbnail(member.user.displayAvatarURL({
-                    dynamic: true
-                }))
-
-            return member.roles.remove(roleName).catch((e) => {
-
-                message.channel.send('An Error occured!' + " " + e);
-
-                embed.setTitle('ERROR')
-                embed.setDescription('>' + " " + e)
-                embed.setColor('RED')
-
-
-
-            }).then(() => message.channel.send(embed));
-
-
-
-
-        } catch (e) {
-
-            const errorembed = new MessageEmbed()
-
-                .setTitle('Error')
-                .setDescription("An unexpected Error occured!")
-                .addField('Make sure', 'you mentioned a role!')
-                .addField('Make sure', 'you got the right order! <user> <role>!')
-                .addField('Developer Info', e)
+            let useembed = new MessageEmbed()
+                .setTitle('Incorrect Usage')
+                .setDescription('Its `<username | user id> <role name>')
                 .setColor('RED')
 
-            return message.channel.send(errorembed).then(() => console.log('An Error occured. Check the error message!'));
-        }
+            let STTpermissions = new MessageEmbed()
+                .setDescription('Please give the Bot Permissions to do this!')
+                .addField('Needed', '`MANAGE_ROLES`')
+                .setColor('RED')
 
+            let nomember = new MessageEmbed()
+                .setDescription('Please mention someone, or provide an ID!')
+                .setColor('RED')
+
+            let norole = new MessageEmbed()
+                .setDescription('Please mention a role!')
+                .setColor('RED')
+
+
+
+            if (!message.guild.me.hasPermission('MANAGE_ROLES')) {
+
+                return message.channel.send(STTpermissions);
+
+            }
+
+            if (!message.channel.id == '587956575315034114') return;
+
+
+            if (!args[0] || !args[1]) return message.channel.send(useembed);
+
+
+            try {
+
+                const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+                const roleName = message.guild.roles.cache.find(r => (r.name === args[1].toString()) || (r.id === args[1].toString().replace(/[^\w\s]/gi, '')));
+
+                if (!member) return message.channel.send(nomember);
+                //if (!roleName) return message.channel.send(norole);
+
+                const alreadyHasRole = member._roles.includes(roleName.id);
+
+                if (!alreadyHasRole) return message.channel.send('This User does not have that Role!');
+
+
+                const embed = new MessageEmbed()
+                    .setTitle(`${roleName.name} was removed!`)
+                    .setDescription(`${roleName} was removed from ${member.user}`)
+                    .setColor('f3f3f3')
+                    .setTimestamp()
+                    .setFooter(`${message.author.username}`)
+                    .setThumbnail(member.user.displayAvatarURL({
+                        dynamic: true
+                    }))
+
+                return member.roles.remove(roleName).catch((e) => {
+
+                    message.channel.send('An Error occured!' + " " + e);
+
+                    embed.setTitle('ERROR')
+                    embed.setDescription('>' + " " + e)
+                    embed.setColor('RED')
+
+
+
+                }).then(() => message.channel.send(embed));
+
+
+
+
+            } catch (e) {
+
+                const errorembed = new MessageEmbed()
+
+                    .setTitle('Error')
+                    .setDescription("An unexpected Error occured!")
+                    .addField('Make sure', 'you mentioned a role!')
+                    .addField('Make sure', 'you got the right order! <user> <role>!')
+                    .addField('Developer Info', e)
+                    .setColor('RED')
+
+                return message.channel.send(errorembed).then(() => console.log('An Error occured. Check the error message!'));
+            }
+        } else {
+
+            console.log('*RETURN was used in the wrong channel!');
+            return;
+
+        }
 
     }
 }
