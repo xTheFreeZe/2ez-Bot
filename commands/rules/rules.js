284e87f8740b31978aed0ac673726940acf829e6
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2021-06-29 19:25:15 +0200

    Rule Patch

---
+++ commands/rules/rules.js
@@ -0,0 +1,47 @@
+const {
+    MessageEmbed
+} = require('discord.js');
+
+
+module.exports = {
+    name: "rules",
+    category: "mot test",
+    description: "test",
+
+    run: async (client, message, testlist) => {
+
+        let ruleembeddec = [
+            "1) Disruptive behaviour is not tolerated. This includes (but is not limited to) spam, slurs, hate speech, harassment, insults, witch-hunting, and impersonation.",
+            " ",
+            "2) NSFW (Not Safe For Work) content is prohibited in all text and voice channels.",
+            " ",
+            "3) The discussion of the following topics are not permitted: Race, Religion, Politics.",
+            " ",
+            "4) If you create a consistently negative experience for others, even if you're not technically breaking the rules in any specific instance, we'll treat it as breaking the rules.",
+            " ",
+            "5) Posting content known to crash Discord or that could cause non-malicious effects to a members PC are prohibited in all text channels.",
+            " ",
+            "6) Advertisement is only permitted in <#!813016558728511499>. Discord invite links are not permitted unless discussed with the management team.",
+            " ",
+            "7) This server is English speaking. We kindly ask that you only use English to communicate with members on this server.",
+            " ",
+            "8) Final verdict and rule interpretation is at management team discretion. If you are asked by an Admin/C-Rep to stop doing something, stop doing it.",
+            "• If you have questions regarding a decision made by either an Admin or C-Rep, please contact us by sending a DM to <@!575252669443211264>.",
+            " ",
+            "Infractions will typically progress as such: warn -> mute -> ban. Steps in this progression may be skipped based on the severity of the infraction.",
+            " ",
+            "In addition to these rules, the management team reserves the right to remove messages and users from the server that are detrimental to the discussion and community.",
+            "Since this is the first page you are seeing, ignorance of the rules does not excuse breaking them."
+        ]
+
+        const rulesembed = new MessageEmbed()
+            .setTitle('2ez Rules')
+            .setDescription(ruleembeddec)
+            .setColor('BLUE')
+            .setThumbnail('https://cdn.discordapp.com/attachments/753238962050695228/813016413131243550/Rules.png')
+
+        message.channel.send(rulesembed);
+
+
+    }
+}
