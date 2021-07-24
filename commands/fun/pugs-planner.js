e097e7ac5ff1e4e0b2c4f18a174f0e1bd5d14918
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2021-07-24 17:43:35 +0200

    Update pugs-planner.js

--- commands/fun/pugs-planner.js
+++ commands/fun/pugs-planner.js
@@ -129,7 +129,7 @@ module.exports = {
 
                     ];
 
-                    userNames.push(user.username);
+                    userNamesNO.push(user.username);
 
                     const editpugs = new MessageEmbed()
                         .setTitle(`${message.author.username}'s unoffical pugs`)
