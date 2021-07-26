22b9ddf344f9aacf962ba1ae7c1e4bbb4fc0859e
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2021-07-26 18:19:18 +0200

    New Command *poll

---
+++ commands/fun/poll.js
@@ -0,0 +1,104 @@
+const {
+    MessageEmbed
+} = require('discord.js');
+
+const Discord = require('discord.js');
+const UserIDCount = new Set();
+
+
+module.exports = {
+
+    name: "poll",
+    run: async (client, message, PREFIX) => {
+
+        var iyes = 0;
+        var xno = 0;
+
+        let args = message.content.substring(PREFIX.length).split(" ");
+
+        let suggestion = args.sclice(1).join("");
+
+        let suggestionembed = new MessageEmbed()
+            .setTitle('New Poll')
+            .setDescription(suggestion)
+            .setFooter(`Suggestion from ${message.author.username}`)
+            .setTimestamp()
+            .setColor('BLUE')
+
+
+        message.channel.send(suggestion)
+            .then(m => {
+                m.react('✅');
+                m.react('❌');
+
+
+                const yesfilter = (reaction, user) => reaction.emoji.name === '✅';
+                const yescollector = m.createReactionCollector(yesfilter, {
+                    max: 100,
+                });
+
+                yescollector.on('collect', async (reaction, user) => {
+
+                    let useravatar = user.displayAvatarURL({
+                        format: 'png',
+                        dynamic: true
+                    });
+
+                    if (user.id === "830087071413567519") return;
+                    reaction.users.remove(user.id);
+                    if (UserIDCount.has(user.id)) return;
+
+                    iyes = iyes + 1 * 1
+
+                    const editsuggestion = new MessageEmbed()
+                        .setTitle(`New Poll`)
+                        .setDescription(suggestion) // ${i}
+                        .addField('Yes', iyes)
+                        .addField('No', xno)
+                        .setTimestamp()
+
+                    UserIDCount.add(user.id);
+                    m.edit(editsuggestion);
+
+
+                });
+
+
+
+                const nofilter = (reaction, user) => reaction.emoji.name === '❌';
+                const nocollector = m.createReactionCollector(nofilter, {
+                    max: 100,
+                });
+
+                nocollector.on('collect', async (reaction, user) => {
+
+                    let useravatar = user.displayAvatarURL({
+                        format: 'png',
+                        dynamic: true
+                    });
+
+                    if (user.id === "830087071413567519") return;
+                    reaction.users.remove(user.id);
+                    if (!UserIDCount.has(user.id)) return;
+
+                    xno = xno - 1 * 1
+
+                    const editsuggestion = new MessageEmbed()
+                        .setTitle(`New Poll`)
+                        .setDescription(suggestion) // ${i}
+                        .addField('Yes', iyes)
+                        .addField('No', xno)
+                        .setTimestamp()
+
+                    UserIDCount.delete(user.id);
+                    m.edit(editsuggestion);
+
+
+                });
+
+
+
+            })
+    }
+
+}
